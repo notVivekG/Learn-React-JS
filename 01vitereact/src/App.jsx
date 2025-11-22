fe339151f08048b91d49415667c2cd29030da960
Vivek Kushwaha <notvivekkushwaha@gmail.com>
2025-11-22 15:41:41 +0530

    upto episode 4 ( custom react )

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,11 @@
 import Vivek from "./vivek"
 
 function App() {
-
+  const username = "vivek kushwaha"
   return (
     <>
       <Vivek />
-      <h1>chai aur react</h1>
+      <h1>chai aur react {username}</h1>
       <p>This is a para</p>
     </>
   );
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,7 +1,24 @@
+import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function Myapp(){
+    return(
+        <div>
+            <h1>Custom App!</h1>
+        </div>
+    )
+}
+
+const reactElement = React.createElement(
+    'a',
+    {href: 'https://google.com', target: '_blank'},
+    'click me to visit  google'
+)
+
 createRoot(document.getElementById('root')).render(
-    <App />
+    // reactElement         , paranthesis is not need as it is a object
+    Myapp()
+    // <App />
 )
