04c20f861917bdabec66436d4ae14324873857d8
Vivek Kushwaha <notvivekkushwaha@gmail.com>
2025-12-03 20:11:33 +0530

    upto episode 14 ( todo context api )

---
+++ 10todocontextLocal/src/contexts/ToDoContext.js
@@ -0,0 +1,22 @@
+import { createContext, useContext } from "react";
+
+export const TodoContext = createContext({
+    todos: [
+        {
+            id: 1,
+            todod: "Todo msg",
+            completed: false,
+        }
+    ],
+    addTodo: (todo) => {},
+    updateTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {},
+})
+
+export const TodoProvider = TodoContext.Provider; 
+
+const useTodo = () => {
+    return useContext(TodoContext)
+}
+export default useTodo;
