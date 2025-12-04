bc9ff50dc0f2a58edc2c4e9f8c41181958bf9966
Vivek Kushwaha <notvivekkushwaha@gmail.com>
2025-12-04 11:26:21 +0530

    fixed minor mistake

--- 10todocontextLocal/src/contexts/ToDoContext.js
+++ 10todocontextLocal/src/contexts/ToDoContext.js
@@ -4,7 +4,7 @@ export const TodoContext = createContext({
     todos: [
         {
             id: 1,
-            todod: "Todo msg",
+            todo: "Todo msg",
             completed: false,
         }
     ],
