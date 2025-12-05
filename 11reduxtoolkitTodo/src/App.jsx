85f6d8979e53dcf7f83c07b2619110c1afb0ffc4
Vivek Kushwaha <notvivekkushwaha@gmail.com>
2025-12-05 22:19:03 +0530

    upto episode 15 ( redux toolkit )

---
+++ 11reduxtoolkitTodo/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
+
+function App() {
+  
+  return (
+    <>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
---
+++ 11reduxtoolkitTodo/src/features/todo/todoSlice.js
@@ -0,0 +1,40 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+    todos: [{
+        id:1,
+        text: "Hello World"
+    }]
+}
+
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState,
+    reducers: {
+        addTodo: (state, action) => {
+            const todo = {
+                id: nanoid(),
+                text: action.payload
+            }
+            state.todos.push(todo)
+        },
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+        },
+        updateTodo: (state, action) => {
+            state.todos = state.todos.map((todo) => (todo.id === action.payload.id ? action.payload : todo ))
+        }
+        // Faster :-
+        //      updateTodo: (state, action) => {
+        //          const index = state.todos.findIndex(todo => todo.id === action.payload.id);
+        //          if (index !== -1) {
+        //              state.todos[index] = action.payload;
+        //          }
+        //      }
+
+    }
+})
+
+export const {addTodo, removeTodo, updateTodo} = todoSlice.actions
+
+export default todoSlice.reducer;
