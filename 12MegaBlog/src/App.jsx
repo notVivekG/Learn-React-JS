b21430cdbab922a2f0b291a90aa93e8b5d18015c
Vivek Kushwaha <notvivekkushwaha@gmail.com>
2025-12-13 15:15:36 +0530

    upto episode 21 ( redux toolkit )

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,12 +1,37 @@
+import React, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css'
-function App() {
-    console.log(import.meta.env.VITE_APPWRITE_URL);
+import authService from './appwrite/auth'
+import { login, logout} from  "./store/authSlice"
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 
-  return (
-    <>
-      <h1>A blog app in appwrite </h1>
-    </>
-  )
+function App() {
+  const [loading, setLoading] = useState(true) // useffect mai false kar denge
+  const dispatch = useDispatch()
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          TODO: {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
