f268c9e4fba618e3217ff8616acee3bcc6215fd4
Vivek Kushwaha <notvivekkushwaha@gmail.com>
2025-12-18 19:12:19 +0530

    upto episode 24 ( RTE & PostForm )

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -3,11 +3,13 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container"
 import Logo from "./Logo"
 import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE.jsx";
 
 export {
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    RTE,
 }
